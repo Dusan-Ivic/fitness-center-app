bb62c8c49690aef3e8f7bc14f8fef85085f1c79f
Dusan-Ivic <ivicd99@gmail.com>
2023-01-11 21:45:11 +0100

    Connect server to database

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const { connectDB } = require("./config/database");
 
 dotenv.config();
 const PORT = process.env.PORT || 5000;
 const NODE_ENV = process.env.NODE_ENV || "development";
+
+connectDB(process.env.MONGO_URI);
 
 const app = express();
 
