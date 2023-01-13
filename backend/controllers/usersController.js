92f5f8f7d737af103619c32f96d9f089cca5e51f
Dusan-Ivic <ivicd99@gmail.com>
2023-01-13 18:34:45 +0100

    Implement controller for base user model

--- backend/controllers/usersController.js
+++ backend/controllers/usersController.js
@@ -1,43 +1,81 @@
+const User = require("../models/userModel");
+
 // @desc    Get all users
 // @route   GET /api/users
 // @access  Public
-exports.getUsers = (req, res) => {
+exports.getUsers = async (req, res) => {
+  const users = await User.find();
+
   res.status(200).json({
     success: true,
-    message: "Get all users",
+    count: users.length,
+    data: users,
   });
 };
 
 // @desc    Create new user
 // @route   POST /api/users
 // @access  Public
-exports.createUser = (req, res) => {
+exports.createUser = async (req, res) => {
+  const user = await User.create(req.body);
+
   res.status(201).json({
     success: true,
-    message: "Create new user",
-    body: req.body,
+    message: "User created",
+    data: user,
   });
 };
 
 // @desc    Update existing user
 // @route   PUT /api/users/:id
 // @access  Public
-exports.updateUser = (req, res) => {
+exports.updateUser = async (req, res) => {
+  const user = await User.findById(req.params.id);
+
+  if (!user) {
+    return res.status(404).json({
+      success: false,
+      message: "User with specified ID doesn't exist",
+    });
+  }
+
+  user.email = req.body.email;
+  user.username = req.body.username;
+  user.password = req.body.password;
+  user.firstName = req.body.firstName;
+  user.lastName = req.body.lastName;
+  user.gender = req.body.gender;
+  user.birthdate = req.body.birthdate;
+
+  await user.save();
+
   res.status(200).json({
     success: true,
-    message: "Update existing user",
-    body: req.body,
-    id: req.params.id,
+    message: "User updated",
+    data: user,
   });
 };
 
 // @desc    Delete existing user
 // @route   DELETE /api/users/:id
 // @access  Public
-exports.deleteUser = (req, res) => {
+exports.deleteUser = async (req, res) => {
+  const user = await User.findById(req.params.id);
+
+  if (!user) {
+    return res.status(404).json({
+      success: false,
+      message: "User with specified ID doesn't exist",
+    });
+  }
+
+  await user.remove();
+
   res.status(200).json({
     success: true,
-    message: "Delete existing user",
-    id: req.params.id,
+    message: "User deleted",
+    data: {
+      _id: req.params.id,
+    },
   });
 };
---
+++ backend/models/userModel.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const userSchema = Schema({
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  username: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  gender: {
+    type: String,
+    required: true,
+  },
+  birthdate: {
+    type: Date,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model("User", userSchema);
