cabbac7991efb8303809bf1f81c50d4b64b52759
Dušan Ivić <76745718+Dusan-Ivic@users.noreply.github.com>
2023-01-16 14:31:36 +0100

    Add validations for body and params (#2)
    * Validate request body for POST and PUT methods
    
    * Validate params for PUT and DELETE methods

--- backend/routes/usersRoutes.js
+++ backend/routes/usersRoutes.js
@@ -1,13 +1,19 @@
 const express = require("express");
 const router = express.Router();
 const usersController = require("../controllers/usersController");
+const userValidator = require("../middleware/validators/userValidator");
 
 router.get("/", usersController.getUsers);
 
-router.post("/", usersController.createUser);
+router.post("/", userValidator.validateBody, usersController.createUser);
 
-router.put("/:id", usersController.updateUser);
+router.put(
+  "/:id",
+  userValidator.validateParams,
+  userValidator.validateBody,
+  usersController.updateUser
+);
 
-router.delete("/:id", usersController.deleteUser);
+router.delete("/:id", userValidator.validateParams, usersController.deleteUser);
 
 module.exports = router;
