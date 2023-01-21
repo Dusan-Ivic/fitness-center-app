79bc2dd6cd227d1bd360679dda4bd0f35f163948
Dusan-Ivic <ivicd99@gmail.com>
2023-01-21 17:18:12 +0100

    Protect Users API endpoints with authentication

--- backend/routes/usersRoutes.js
+++ backend/routes/usersRoutes.js
@@ -2,18 +2,25 @@ const express = require("express");
 const router = express.Router();
 const usersController = require("../controllers/usersController");
 const userValidator = require("../middleware/validators/userValidator");
+const authMiddleware = require("../middleware/authMiddleware");
 
-router.get("/", usersController.getUsers);
+router.get("/", authMiddleware.authenticateUser, usersController.getUsers);
 
 router.post("/", userValidator.validateBody, usersController.createUser);
 
 router.put(
   "/:id",
+  authMiddleware.authenticateUser,
   userValidator.validateParams,
   userValidator.validateBody,
   usersController.updateUser
 );
 
-router.delete("/:id", userValidator.validateParams, usersController.deleteUser);
+router.delete(
+  "/:id",
+  authMiddleware.authenticateUser,
+  userValidator.validateParams,
+  usersController.deleteUser
+);
 
 module.exports = router;
