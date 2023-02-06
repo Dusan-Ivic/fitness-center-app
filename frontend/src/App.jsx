cacbcd6d61b6b943b5289d72d47578103e2cbac2
Dusan-Ivic <ivicd99@gmail.com>
2023-02-06 13:20:50 +0100

    Add basic layout with navbar and footer

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,23 @@
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Container from "react-bootstrap/Container";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
 
 function App() {
   return (
-    <div className="App">Frontend for Fitness Center Information System</div>
+    <>
+      <Router>
+        <Header />
+        <Container className="mt-3">
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+          </Routes>
+        </Container>
+        <Footer />
+      </Router>
+    </>
   );
 }
 
---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "../assets/styles/Footer.css";
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <p>
+        <a className="author" href="https://github.com/Dusan-Ivic/">
+          Dušan Ivić
+        </a>{" "}
+        &copy; 2023
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
