9f1d16a04aff74326642223609fd1fa67b004b40
Dusan-Ivic <ivicd99@gmail.com>
2023-02-07 14:21:28 +0100

    Create register form template

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,8 +2,9 @@ import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Container from "react-bootstrap/Container";
 import Header from "./components/Header";
-import Footer from "./components/Footer";
+// import Footer from "./components/Footer";
 import Home from "./pages/Home";
+import Register from "./pages/Register";
 
 function App() {
   return (
@@ -13,9 +14,10 @@ function App() {
         <Container className="mt-3">
           <Routes>
             <Route exact path="/" element={<Home />} />
+            <Route path="/register" element={<Register />} />
           </Routes>
         </Container>
-        <Footer />
+        {/* <Footer /> */}
       </Router>
     </>
   );
