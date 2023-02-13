be975e27bc34509d01f0924bc81a52e96364b96d
Dusan-Ivic <ivicd99@gmail.com>
2023-02-13 20:30:10 +0100

    Create template for user profile page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,6 +7,7 @@ import Header from "./components/Header";
 import HomePage from "./pages/HomePage/HomePage";
 import RegisterPage from "./pages/RegisterPage/RegisterPage";
 import LoginPage from "./pages/LoginPage/LoginPage";
+import ProfilePage from "./pages/ProfilePage/ProfilePage";
 
 function App() {
   return (
@@ -19,6 +20,7 @@ function App() {
             <Route exact path="/" element={<HomePage />} />
             <Route path="/register" element={<RegisterPage />} />
             <Route path="/login" element={<LoginPage />} />
+            <Route path="/profile" element={<ProfilePage />} />
           </Routes>
         </Container>
       </Router>
---
+++ frontend/src/pages/ProfilePage/ProfilePage.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+import Card from "react-bootstrap/Card";
+import ListGroup from "react-bootstrap/ListGroup";
+import "./ProfilePage.css";
+import { useSelector } from "react-redux";
+import moment from "moment";
+
+const ProfilePage = () => {
+  const { user } = useSelector((state) => state.users);
+
+  return (
+    <div>
+      <h1>Profile</h1>
+      <Card className="details">
+        <ListGroup variant="flush">
+          <ListGroup.Item className="details-item">
+            <div className="details-item-title">Email</div>
+            <div className="details-item-value">{user.email}</div>
+          </ListGroup.Item>
+          <ListGroup.Item className="details-item">
+            <div className="details-item-title">Username</div>
+            <div className="details-item-value">{user.username}</div>
+          </ListGroup.Item>
+          <ListGroup.Item className="details-item">
+            <div className="details-item-title">First name</div>
+            <div className="details-item-value">{user.firstName}</div>
+          </ListGroup.Item>
+          <ListGroup.Item className="details-item">
+            <div className="details-item-title">Last name</div>
+            <div className="details-item-value">{user.lastName}</div>
+          </ListGroup.Item>
+          <ListGroup.Item className="details-item">
+            <div className="details-item-title">Gender</div>
+            <div className="details-item-value">{user.gender}</div>
+          </ListGroup.Item>
+          <ListGroup.Item className="details-item">
+            <div className="details-item-title">Birthdate</div>
+            <div className="details-item-value">
+              {moment(user.birthdate).format("LL")}
+            </div>
+          </ListGroup.Item>
+          <ListGroup.Item className="details-item">
+            <div className="details-item-title">Role</div>
+            <div className="details-item-value">{user.role}</div>
+          </ListGroup.Item>
+        </ListGroup>
+      </Card>
+    </div>
+  );
+};
+
+export default ProfilePage;
